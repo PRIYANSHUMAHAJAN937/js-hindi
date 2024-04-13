be65da5c4d1911cced54e45ea4c7b097f351ff19
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-13 07:17:30 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const name = "Priyanshu"
+const repoCount= 8
+//console.log(name + repoCount +" Value ")
+
+//console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+
+const gameName = new String('PMriyanshu')
+const nam1 = new String('Priya-nshu')
+
+//console.log(gameName[0])
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+console.log(gameName.charAt(6));
+console.log(gameName.indexOf('s'))
+
+const newString = nam1.substring(0,5)
+console.log(newString)
+const  anotherString = nam1.slice(-8,4)
+console.log(anotherString)
+
+const string2 = "   tommy    "
+console.log(string2)
+console.log(string2.trim());
+
+const url = "https://priyansh.com"
+console.log(url.replace('a','x'))
+console.log(url)
+console.log(url.includes('star'))
+console.log(nam1.split('-'));
+
