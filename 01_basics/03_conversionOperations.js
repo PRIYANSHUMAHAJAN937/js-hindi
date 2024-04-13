084114d2a7d051bece2d81f9744c4e037558966b
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-13 04:06:46 +0000

    comparison

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -59,3 +59,4 @@ let gameCounter = 100
 gameCounter++
 ++gameCounter
 console.log(gameCounter)
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+//console.log(2 > 1)true
+//console.log(2 >= 1);true
+//console.log(2 < 1)false
+//console.log(2 == 1)false
+//console.log(2 != 1)true
+
+console.log("2" > 1) // true
+console.log("02" > 1)//true
+
+console.log(null > 0) // false
+console.log(null == 0) //false
+console.log(null >= 0)//true  // null 0 me convert ho jaata hai
+// do not use this
+// avoid these comparisons write clean and understandable code
+// if we do samething with undefined we will always get false
+
+// === (strict check)
+
+console.log("2" === 2); // === checks datatype also
