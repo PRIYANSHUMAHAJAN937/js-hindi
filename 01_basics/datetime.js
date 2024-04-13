aefe277cb0da1762176e599beef82916c9dd743d
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-13 10:40:35 +0000

    dateandtime

---
+++ 01_basics/datetime.js
@@ -0,0 +1,32 @@
+let myDate  = new Date()
+//console.log(myDate)
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+//console.log(myDate.toISOString())
+//console.log(myDate.toJSON())
+//console.log(myDate.toLocaleDateString())
+//console.log(myDate.toLocaleString())
+//console.log(myDate.toLocaleTimeString())\
+
+//let myCreateDate = new Date(2024, 0, 23)
+//console.log(myCreateDate.toDateString())
+//let myCreateDate = new Date(2024, 0, 23,5,3)
+//console.log(myCreateDate.toDateString())
+//let myCreateDate = new Date("2024-04-16")
+let myCreateDate = new Date("01-14-2024")
+//console.log(myCreateDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp)
+
+//console.log(myCreateDate.getTime())
+
+//console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
+
+
+
