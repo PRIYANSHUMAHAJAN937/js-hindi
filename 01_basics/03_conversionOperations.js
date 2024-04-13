afd55f9fa4b7a2959cbcca73f06e6739899395d6
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-13 03:52:50 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -9,17 +9,53 @@
 //"33abc" => NaN
 //true =1,false = 0
 
-let isLoggedIn = "priyanshu"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//let isLoggedIn = "priyanshu"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1-> true; 0->false
 // ""->false
 // "priyanshu"->true
 
-let num =44
-let stringNum = String(num)
-console.log(stringNum)
+//let num =44
+//let stringNum = String(num)
+//console.log(stringNum)
 
+//*******************************operations**************************
 
+let value=3
+let negValue = -value;
+//console.log(negValue)
+/*console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)*/
 
+let str1 = "hello"
+let str2 = " Priyanshu"
+let str3 = str1+str2
+//console.log(str3);
+
+//console.log("1" + 2)
+//console.log(1 + "2")
+//console.log("1" + "2")
+//console.log("1" + 2 + 2) // the datatype which is at beggining decides
+//datatype of entire operation
+//console.log(1 + 2 + "2")
+
+//console.log((3 + 4) *5 % 3)
+
+//console.log(true)//true
+//console.log(+true)//1
+//console.log(true+)//error
+//console.log(+"") //0  avoid this
+
+let num1, num2, num3 
+//num1 = num2 = num3 = 2 + 2//=4
+
+let gameCounter = 100
+gameCounter++
+++gameCounter
+console.log(gameCounter)
