750dcf966c82eff4a727d5988775f9930abdbb01
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-13 05:16:13 +0000

    primitive / nonprimitive datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,15 @@
+// Ther are two types of datatypes primitive and non-primitive
+// on the basis of howw the data is stored and accessed
+
+// primitive   
+
+//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id==anotherId)//false
+//Non primitive or reference type
+
+//Array, Objects, Functions
+const heroes=["thor","ironman","hulk"]
