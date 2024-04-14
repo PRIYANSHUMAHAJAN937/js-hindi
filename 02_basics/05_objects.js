223266d8d5c7e084d14a7c90e7ca67a7b45b4d65
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-14 07:30:48 +0000

    objectspart3

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,18 @@
+const course  = {
+    cname: "Javascript",
+    price: "999",
+    courseinstructor:"codewithchai"
+}
+
+//course.courseinstructor
+
+const {courseinstructor: instructor} = course
+//console.log(courseinstructor)
+console.log(instructor)
+
+//{       
+     /*this is json */
+//}
+
+
+
