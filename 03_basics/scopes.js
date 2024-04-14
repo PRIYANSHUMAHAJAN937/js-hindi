c0d6566b190807d1a295612c76ab7e31bf24bad8
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-14 16:02:58 +0000

    scopes

---
+++ 03_basics/scopes.js
@@ -0,0 +1,15 @@
+
+///var c = 300
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+    //var c = 30  // avoid var
+    console.log(a)
+
+}
+
+
+console.log(a)
+//console.log(b)
+//console.log(c) //yr print ho jaata , scpe se bahr aa jaata hai
