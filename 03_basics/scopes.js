2060cf525614a601d489e9cdc5fde445b0222e7b
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-16 16:33:57 +0000

    scopes

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -1,15 +1,50 @@
 
 ///var c = 300
-let a = 300
-if(true){
-    let a = 10
-    const b = 20
+//let a = 300
+//if(true){
+   // let a = 10
+   // const b = 20
     //var c = 30  // avoid var
-    console.log(a)
+    //console.log(a)
 
+//}
+
+
+//console.log(a)
+//console.log(b)
+//console.log(c) //yr print ho jaata , scpe se bahr aa jaata hai
+
+//function one(){
+    //const username = "hitesh"
+
+    //function two(){
+    //    const website = "youtube"
+    //    console.log(username);
+    //}
+    //console.log(website)  error ayega
+
+    //two()
+//}
+//one()
+
+//if(true){
+    //const username = "priyanshu"
+    //if(username=="priyanshu"){
+      //  const website = "youtube"
+     //   console.log(username+website)
+    //}
+    //console.log(website) error
+//}
+//console.log(username) error
+
+console.log(addOne(5))//ye chlega
+function addOne(num){
+    return num+1
+}
+
+console.log(addTwo(5)) // but this will cause error
+const addTwo = function(num){
+    return num+2
 }
 
 
-console.log(a)
-//console.log(b)
-//console.log(c) //yr print ho jaata , scpe se bahr aa jaata hai
