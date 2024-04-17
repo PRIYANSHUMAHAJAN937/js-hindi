1af08a71115e5a5fd341bd0527626558f4055020
Priyanshu <103842872+PRIYANSHUMAHAJAN937@users.noreply.github.com>
2024-04-17 05:55:00 +0000

    arrow and this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,52 +3,45 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
+        console.log(`${this.username} , welcome to website`); // this is used to refer or access to current context
         console.log(this);
     }
-
 }
 
-// user.welcomeMessage()
-// user.username = "sam"
-// user.welcomeMessage()
+//user.welcomeMessage()
+//user.username = "sam"
+//user.welcomeMessage()
 
-// console.log(this);
+//console.log(this); //if we run this here we get {}(empty object)  , but when we run this in browser we get window object because it is the global object
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+//function chai(){
+  //   let username = "hitesh"
+    // console.log(this.username); //undefined
+ //}
 
-// chai()
+//chai()
 
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+//const chai = function () {
+  // let username = "hitesh"
+   //console.log(this.username); //undefined
+ //}
 
-const chai =  () => {
-    let username = "hitesh"
+const chai =  () => { // everything same,just remove function keyword and add arrow after parenthesis
+  let username = "hitesh"
     console.log(this);
 }
-
-
-// chai()
+ //chai()
 
 // const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
+  //   return num1 + num2
+ //}
 
-// const addTwo = (num1, num2) =>  num1 + num2
+ //const addTwo = (num1, num2) =>  num1 + num2  //implicit return
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
+//const addTwo = (num1, num2) => ( num1 + num2 )
 
 const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
 console.log(addTwo(3, 4))
 
-
-// const myArray = [2, 5, 3, 7, 8]
-
-// myArray.forEach()
